884d895fa469339282e142046523735aebd4a284
sangha110495 <48673702+sangHa0411@users.noreply.github.com>
2021-09-30 14:59:38 +0900

    apply entity marker method for re task

--- load_data.py
+++ load_data.py
@@ -52,19 +52,34 @@ class RE_Dataset(Dataset):
     return train_dset, val_dset
 
 def preprocessing_dataset(dataset):
+  """ 처음 불러온 csv 파일을 원하는 형태의 DataFrame으로 변경 시켜줍니다."""
   subject_entity = []
   object_entity = []
-  for i, j in zip(dataset['subject_entity'], dataset['object_entity']):
-    i = eval(i)['word']
-    j = eval(j)['word']
-    subject_entity.append(i)
-    object_entity.append(j)
+  sen_data = []
+  for s, i, j in zip(dataset['sentence'], dataset['subject_entity'], dataset['object_entity']):
+    sub_info=eval(i)
+    obj_info=eval(j)
+    
+    subject_entity.append(sub_info['word'])
+    object_entity.append(obj_info['word'])
+        
+    sub_type = 'SUB'
+    sub_start = sub_info['start_idx']
+    sub_end = sub_info['end_idx']
+    obj_type = 'OBJ'
+    obj_start = obj_info['start_idx']
+    obj_end = obj_info['end_idx']
+        
+    sen = add_sep_tok(s, sub_start, sub_end, sub_type, obj_start, obj_end, obj_type)
+    sen_data.append(sen)
 
   out_dataset = pd.DataFrame({'id':dataset['id'], 
-    'sentence':dataset['sentence'],
+    'sentence':sen_data,
     'subject_entity':subject_entity,
     'object_entity':object_entity,
-    'label':dataset['label'],})
+    'label':dataset['label'],}
+  )
+  
   return out_dataset
 
 def load_data(dataset_dir):
@@ -72,16 +87,34 @@ def load_data(dataset_dir):
   dataset = preprocessing_dataset(pd_dataset)
   return dataset
 
+def add_sep_tok(sen, sub_start, sub_end, sub_type, obj_start, obj_end, obj_type) :
+  sub_sos = ' ['+sub_type+'_SOS] '
+  sub_eos = ' ['+sub_type+'_EOS] '
+
+  obj_sos = ' ['+obj_type+'_SOS] '
+  obj_eos = ' ['+obj_type+'_EOS] '
+
+  if sub_start < obj_start :
+    sen = sen[:sub_start] + sub_sos + sen[sub_start:sub_end+1] + sub_eos + sen[sub_end+1:]
+    obj_start += 22
+    obj_end += 22
+    sen = sen[:obj_start] + obj_sos + sen[obj_start:obj_end+1] + obj_eos + sen[obj_end+1:]
+  else :
+    sen = sen[:obj_start] + obj_sos + sen[obj_start:obj_end+1] + obj_eos + sen[obj_end+1:]
+    sub_start += 22
+    sub_end += 22
+    sen = sen[:sub_start] + sub_sos + sen[sub_start:sub_end+1] + sub_eos + sen[sub_end+1:]
+  return sen
+
+# is preprocessor really useful
 def tokenized_dataset(dataset, tokenizer, maxlen):
   entity_data = []
   sen_data = []
   for e01, e02, s in zip(dataset['subject_entity'], dataset['object_entity'], dataset['sentence']):
     entity = e01 + ' [SEP] ' + e02
-    entity = preprocess_sen(entity)
-    sen = preprocess_sen(s)
 
     entity_data.append(entity)
-    sen_data.append(sen)
+    sen_data.append(s)
 
   tokenized_sentences = tokenizer(
     entity_data,
