fa9d7d06643c573c7be941ff08174df00fc822f2
sangha110495 <48673702+sangHa0411@users.noreply.github.com>
2021-10-02 08:32:14 +0900

    Update preprocessor.py

--- preprocessor.py
+++ preprocessor.py
@@ -1,8 +1,28 @@
 import re
 
-def preprocess_sen(sen) :
-    sen = re.sub('[ぁ-ㄬ㐀-鿕]+', ' [CHN] ' , sen)
-    sen = re.sub('[^가-힣0-9a-zA-Z\[\]\',.!?]' , ' ', sen)
-    sen = re.sub(' {2,}' , ' ' , sen)
-    sen = sen.strip()
-    return sen
+class Preprocessor :
+    def __init__(self, tokenizer) :
+        self.kor_processor = re.compile(self.buile_list(tokenizer))
+    
+    def buile_list(self, tokenizer) :
+        kor_chars = []
+        start_idx = ord('가')
+        end_idx = ord('힣')
+
+        for i in range(start_idx, end_idx+1) :
+            ch = chr(i)
+            if tokenizer.convert_tokens_to_ids(ch) == 1 :
+                kor_chars.append(ch)
+
+        kor_ch_list = '[' + ''.join(kor_chars) + ']'
+        return kor_ch_list
+
+    def add_space(self, match) :
+        sep_ch = match.group()
+        return ' ' + sep_ch + ' '
+
+    def __call__(self, sen) :
+        assert isinstance(sen, str)
+        sen = re.sub('[À-ͳẠ-ỹΑ-ΰか-ヿ]' , '' , sen)
+        sen = self.kor_processor.sub(self.add_space, sen)
+        return sen
